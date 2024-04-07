f3104a9b2c30579332b9db359c5d52f8fa8a4d41
Takashi Kato <tohosaku@users.osdn.me>
2024-04-07 10:04:44 +0900

    Remove unneeded config

--- init.sh
+++ init.sh
@@ -13,8 +13,5 @@ ln -s ~/.dotfiles/gitmux.conf ~/.gitmux.conf
 rm -rf ~/.vim
 ln -s ~/.dotfiles/vim ~/.vim
 
-rm -rf ~/.config
-ln -s ~/.dotfiles/config ~/.config
-
 rm -f ~/.tigrc
 ln -s ~/.dotfiles/tigrc ~/.tigrc
