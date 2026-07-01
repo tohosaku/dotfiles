4e16f4bbe6d95edff49a7878e54ee0e890c8bd92
Takashi Kato <tohosaku@cosmichorror.org>
2026-07-01 11:31:21 +0000

    Add vim-matchup

--- config/nvim/lua/plugins/fern.lua
+++ config/nvim/lua/plugins/fern.lua
@@ -1,11 +1,19 @@
 return { "lambdalisue/fern.vim",
   config = function()
     -- Ctrl+nでファイルツリーを表示/非表示する
-    vim.keymap.set('n', '<C-n>', ':Fern . -reveal=% -drawer -toggle -width=40<CR>')
+    vim.keymap.set('n', '<C-n>', ':Fern . -reveal=% -drawer -keep -toggle -width=40<CR>')
 
     -- 隠しフォルダ表示
     vim.g['fern#default_hidden'] = 1
     -- 表示しないファイル
     vim.g['fern#default_exclude'] = [[^\%(\.git\|.*\.swp\|\.jj\)$]]
+
+    -- 以下、Claude による
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "fern",
+      callback = function()
+        vim.opt_local.winfixbuf = true
+      end,
+    })
   end
 }
